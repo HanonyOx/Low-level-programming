d60306a5cb0bde2a3a6477d52a40649d4e1fb905
HanonyOx <tonyomehukwuji@gmail.com>
2025-07-07 12:07:49 +0200

    Edited Assignment1.c and Assignment2.c using vi

---
+++ Assignment1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() { // main function
+
+
+int a = 2910;
+int b = 1000;
+int Multiply = a * b;
+
+
+printf("The result of %d * %d is = %d",Multiply);
+
+  
+
+  return 0;
+}
---
+++ Assignment2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() { // main function
+
+
+int a = 1002983;
+int b = 100000;
+int Divide = a % b;
+
+
+printf("The result of %d % %d is = %d\", Divide);
+
+  
+
+  return 0;
+}
